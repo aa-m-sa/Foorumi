2181d7961a0144406318a91a371b6861f35d63a9
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-12-11 11:16:01 +0200

    initial client-side functionality that uses topic-api (w7-51)

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,21 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api){
   // Toteuta kontrolleri tähän
+  Api.getTopics().success(function(topicdata, status, headers, config) {
+    console.log('successfully got topics');
+    $scope.topics = topicdata;
+  }).error(function(data, status, headers, config) {
+    console.log('error getting topics, status: ' + status);
+  });
+
+  $scope.addTopic = function () {
+    var tobj = {
+      name: $scope.newTopic.name,
+      description: $scope.newTopic.description
+    };
+    Api.addTopic(tobj).success(function (data, status, headers, config) {
+      console.log('Successfully added topic.');
+      console.log('Redirect to /topics/' + data.id);
+      $location.path('/topics/' + data.id);
+    });
+  };
 });
