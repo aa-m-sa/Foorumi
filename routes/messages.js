53652a6ae9499afd90c2ec9e78eb0a96474a478c
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-12-16 19:14:25 +0200

    w7-52 (Viestit ja niiden topicit API)

--- routes/messages.js
+++ routes/messages.js
@@ -10,7 +10,13 @@ var Models = require('../models');
 router.get('/:id', function(req, res, next) {
   // Hae viesti tällä id:llä ja siihen liittyvät vastaukset tässä (Vinkki: findOne ja sopiva include)
   var messageId = req.params.id;
-  res.send(200);
+  Models.Message.findAll({
+    where: {id: messageId},
+    include: [Models.Reply]
+  })
+  .then( function (message) {
+    res.json(message);
+  });
 });
 
 // POST /messages/:id/reply
@@ -19,8 +25,12 @@ router.post('/:id/reply', function(req, res, next){
   var messageId = req.params.id;
   // ...tämä vastaus (Vinkki: lisää ensin replyToAdd-objektiin kenttä MessageId, jonka arvo on messageId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
   var replyToAdd = req.body;
+  replyToAdd.MessageId = messageId;
   // Palauta vastauksena lisätty vastaus
-  res.send(200);
+  Models.Reply.create(replyToAdd)
+  .then( function (reply) {
+    res.json(reply);
+  });
 });
 
 module.exports = router;
